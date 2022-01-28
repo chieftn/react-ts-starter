00848ced3d50875c867d495c3f4659bcbf4ee0a7
Rick Kessler <rkessler@microsoft.com>
2022-01-28 12:05:56 -0800

    two types of test demonstrations.

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,7 @@
+import { getMockT } from './src/shared/utils/testUtils';
+
 jest.mock('react-i18next', () => ({
     useTranslation: () => ({
-      t: (key: string, props: object) => `${key}_${JSON.stringify(props)}`
+      t: getMockT()
     })
 }));
---
+++ src/shared/utils/testUtils.ts
@@ -0,0 +1,5 @@
+import type { TFunction } from 'i18next';
+
+export const getMockT = (): TFunction => {
+    return (...args) => args.map((s: any) => JSON.stringify(s)).reduce((p: string, c: string) => `${p} ${c}`);
+}
