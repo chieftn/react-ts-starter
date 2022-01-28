56f92f8abef291d2bea621442d13c08248894385
Rick Kessler <rkessler@microsoft.com>
2022-01-28 13:13:15 -0800

    code coverage and name alignment.

--- jest.config.ts
+++ jest.config.ts
@@ -20,7 +20,13 @@ export default {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/index.tsx",
+    "!src/localization/i18n.ts",
+    "!src/shared/constants.ts",
+    "!src/configuration/**/*"
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
