81511cc2006c3a32d03c34730e59a2972c9e50c3
Rick Kessler <rkessler@microsoft.com>
2023-05-01 18:49:45 -0700

    conditional display

--- src/apps/account/components/account.tsx
+++ src/apps/account/components/account.tsx
@@ -2,8 +2,13 @@ import * as React from 'react';
 import { useMsal, AuthenticatedTemplate, UnauthenticatedTemplate } from '@azure/msal-react';
 import { Link } from '@fluentui/react-components';
 import { loginRequest } from '../../../configuration/authConfig';
+import { appConfig } from '../../../configuration/appconfig';
 
 export const Account: React.FC = () => {
+    if (appConfig.authorizationMode !== 'msal') {
+        return <></>;
+    }
+
     const { instance } = useMsal();
 
     let activeAccount;
